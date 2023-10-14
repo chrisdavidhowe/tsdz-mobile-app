801084692f15c32065a1e36e4a024edf0856ee1e
Chris Howe <chowe@uaudio.com>
2023-10-14 09:03:24 -0400

    two page approach

--- TongshengEBike/App.tsx
+++ TongshengEBike/App.tsx
@@ -9,13 +9,15 @@ import {
 } from 'react-native';
 import Slider, {SliderProps} from '@react-native-community/slider';
 import RadioGroup from 'react-native-radio-buttons-group';
-
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {BleManager} from 'react-native-ble-plx';
 
 export interface SliderParameterProps extends SliderProps {
   parameterName: string;
   parameterIndex: number;
 }
+
+export type DisplayView = 'settings' | 'dashboard';
 
 const SliderComponent = (props: SliderParameterProps) => {
   const [value, setValue] = useState(props.value ?? 0);
@@ -42,14 +44,44 @@ const SliderComponent = (props: SliderParameterProps) => {
 
 function App(): JSX.Element {
   const buttonSize = 40;
+  let bleConnected = false;
+
+  const viewButtons = useMemo(
+    () => [
+      {
+        id: 'dashboard',
+        label: 'DASHBOARD',
+        color: 'black',
+        labelStyle: {fontSize: 17, color: 'black'},
+        containerStyle: {width: '45%', padding: 10, backgroundColor: '#d7dae0'},
+        value: 'dashboard',
+        size: 20,
+      },
+      {
+        id: 'settings',
+        label: 'SETTINGS',
+        value: 'settings',
+        color: 'black',
+        labelStyle: {fontSize: 17, color: 'black'},
+        containerStyle: {width: '45%', padding: 10, backgroundColor: '#d7dae0'},
+        size: 20,
+      },
+    ],
+    [],
+  );
+  const [viewMode, setViewMode] = useState<string | undefined>();
+  useEffect(() => {
+    console.log(`view mode has changed to: ${viewMode}`);
+  }, [viewMode]); // The second argument is an array of dependencies
+
   const assistButtons = useMemo(
     () => [
       {
         id: '1',
         label: 'OFF',
         color: 'black',
-        labelStyle: {fontSize: 25, color: 'black'},
-        containerStyle: {width: '95%', backgroundColor: 'white', padding: 10},
+        labelStyle: {fontSize: 35, color: 'black'},
+        containerStyle: {width: '95%', backgroundColor: 'white', padding: 20},
         value: 'off',
         size: buttonSize,
       },
@@ -58,8 +90,8 @@ function App(): JSX.Element {
         label: 'LOW',
         value: 'low',
         color: 'black',
-        labelStyle: {fontSize: 25, color: 'black'},
-        containerStyle: {width: '95%', backgroundColor: '#53af55', padding: 10},
+        labelStyle: {fontSize: 35, color: 'black'},
+        containerStyle: {width: '95%', backgroundColor: '#53af55', padding: 20},
         size: buttonSize,
       },
       {
@@ -67,8 +99,8 @@ function App(): JSX.Element {
         label: 'MEDIUM',
         value: 'med',
         color: 'black',
-        labelStyle: {fontSize: 25, color: 'black'},
-        containerStyle: {width: '95%', backgroundColor: '#fff271', padding: 10},
+        labelStyle: {fontSize: 35, color: 'black'},
+        containerStyle: {width: '95%', backgroundColor: '#fff271', padding: 20},
         size: buttonSize,
       },
       {
@@ -76,8 +108,8 @@ function App(): JSX.Element {
         label: 'HIGH',
         value: 'high',
         color: 'black',
-        labelStyle: {fontSize: 25, color: 'black'},
-        containerStyle: {width: '95%', backgroundColor: '#f2963b', padding: 10},
+        labelStyle: {fontSize: 35, color: 'black'},
+        containerStyle: {width: '95%', backgroundColor: '#f2963b', padding: 20},
         size: buttonSize,
       },
       {
@@ -85,8 +117,8 @@ function App(): JSX.Element {
         label: 'MAX',
         value: 'max',
         color: 'black',
-        labelStyle: {fontSize: 25, color: 'black'},
-        containerStyle: {width: '95%', backgroundColor: '#ff4e55', padding: 10},
+        labelStyle: {fontSize: 35, color: 'black'},
+        containerStyle: {width: '95%', backgroundColor: '#eb6d6d', padding: 20},
         size: buttonSize,
       },
     ],
@@ -102,39 +134,61 @@ function App(): JSX.Element {
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    height: '100%',
   };
+
+  let batteryPct = 55;
+  let watts = 10;
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <Text style={styles.header}>EBike</Text>
-      <Text style={styles.text}>PEDAL ASSIST</Text>
-      <RadioGroup
-        radioButtons={assistButtons}
-        onPress={setPedalAssistMode}
-        selectedId={pedalAssistMode}
-        layout="column"
-      />
-      <Text style={styles.text}>PARAMETERS</Text>
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <SliderComponent
-          parameterName="EBIKE thing 1"
-          parameterIndex={0}
-          step={1}
-          value={-30}
-          minimumValue={-80}
-          maximumValue={0}
+      {viewMode === 'dashboard' && (
+        <View>
+          <Text style={styles.text}>PEDAL ASSIST</Text>
+          <RadioGroup
+            radioButtons={assistButtons}
+            onPress={setPedalAssistMode}
+            selectedId={pedalAssistMode}
+            layout="column"
+          />
+        <Text style={styles.largeHeader}>Battery : {batteryPct}%</Text>
+        <Text style={styles.largeHeader}>Watts : {watts}</Text>
+        </View>
+      )}
+      {viewMode === 'settings' && (
+        <View>
+          <Text style={styles.text}>PARAMETERS</Text>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={backgroundStyle}>
+            <SliderComponent
+              parameterName="EBIKE thing 1"
+              parameterIndex={0}
+              step={1}
+              value={-30}
+              minimumValue={-80}
+              maximumValue={0}
+            />
+            <SliderComponent
+              parameterName="EBIKE thing 2"
+              parameterIndex={0}
+              step={0.1}
+              value={10}
+              minimumValue={0}
+              maximumValue={10}
+            />
+          </ScrollView>
+        </View>
+      )}
+      <View style={styles.footer}>
+        <RadioGroup 
+          radioButtons={viewButtons}
+          onPress={setViewMode}
+          selectedId={viewMode}
+          layout="row"
         />
-        <SliderComponent
-          parameterName="EBIKE thing 2"
-          parameterIndex={0}
-          step={0.1}
-          value={10}
-          minimumValue={0}
-          maximumValue={10}
-        />
-      </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
@@ -158,7 +212,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#d7dae0',
     marginTop: 10,
     width: '90%',
-    marginLeft: '5%'
+    marginLeft: '5%',
   },
   highlight: {
     fontWeight: '700',
@@ -183,6 +237,19 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     margin: 0,
     color: '#666e80',
+    padding: 10,
+  },
+  largeHeader: {
+    fontSize: 35,
+    textAlign: 'center',
+    fontWeight: '700',
+    margin: 0,
+    color: '#666e80',
+    padding: 15,
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 40,
   },
 });
 
